73c3a14e4a1bc0c064c40747f5e4082a82cbe336
Prabhakar <prabhakarrai173@gmail.com>
2026-03-09 10:16:11 +0530

    spiralMatrix

---
+++ ArrayBased/spiralMatrix.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<vector<int>> mat={
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16},
+        {17,18,19,20}
+    };
+
+    int r=mat.size();
+    int c=mat[0].size();
+
+    int top=0;
+    int bottom=r-1;
+    int left=0;
+    int right=c-1;
+
+    while(top<=bottom && left<=right){
+
+        // top row
+        for(int i=left;i<=right;i++){
+            cout<<mat[top][i]<<" ";
+        }
+        top++;
+
+        // right column
+        for(int i=top;i<=bottom;i++){
+            cout<<mat[i][right]<<" ";
+        }
+        right--;
+
+        // bottom row
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                cout<<mat[bottom][i]<<" ";
+            }
+            bottom--;
+        }
+
+        // left column
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                cout<<mat[i][left]<<" ";
+            }
+            left++;
+        }
+    }
+}
